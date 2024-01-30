ee32d52f2e97b66d826bd17f73d42a6135d4de34
crossroads30 <crossroads30@mail.ru>
2024-01-30 23:57:20 +0300

    feat: add styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,58 @@
 import styled from 'styled-components'
 import Links from './Links'
+import { GiHamburgerMenu } from 'react-icons/gi'
 
 const Navbar = () => {
 	return (
-			<Wrapper>
+		<Wrapper>
+			<div className='section-center flex header'>
 				<h1>перекрёсток</h1>
-        <Links />
-			</Wrapper>
+				<Links />
+				<GiHamburgerMenu className='burger' />
+			</div>
+		</Wrapper>
 	)
 }
+
 export default Navbar
 
 const Wrapper = styled.header`
-	height: 5rem;
-	background-color: wheat;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 3rem;
-  h1 {
-    font-size: 1.5rem;
-    text-transform: uppercase;
-  }
+	background-color: var(--clr-sepia);
+	.header {
+		height: 2rem;
+		/* width: 90%; */
+		h1 {
+			font-size: 0.8rem;
+			text-transform: uppercase;
+			text-shadow: 1px 2px 3px #ff8801;
+		}
+	}
+	.burger {
+		cursor: pointer;
+	}
+	@media screen and (min-width: 768px) {
+		.header {
+			height: 4rem;
+			h1 {
+				font-size: 1.2rem;
+				text-transform: uppercase;
+			}
+			.burger {
+				width: 25px;
+				height: 25px;
+			}
+		}
+	}
+	@media screen and (min-width: 1024px) {
+		.header {
+			height: 5rem;
+			h1 {
+				font-size: 1.5rem;
+				text-transform: uppercase;
+			}
+			.burger {
+				display: none;
+			}
+		}
+	}
 `
