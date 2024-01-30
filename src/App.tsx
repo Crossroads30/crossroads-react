4fed3e351872569b4cb0cd62a0dfde3900874240
crossroads30 <crossroads30@mail.ru>
2024-01-30 15:06:21 +0300

    feat: add routes to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Home, Videos, Songs, Albums, News } from './pages'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 function App(): JSX.Element {
-	return <h1>perekrestok</h1>
+	return (
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/videos' element={<Videos />} />
+				<Route path='/songs' element={<Songs />} />
+				<Route path='/songs/:id' element={<Songs />} />
+				<Route path='/albums' element={<Albums />} />
+				<Route path='/news' element={<News />} />
+			</Routes>
+			<Footer />
+		</BrowserRouter>
+	)
 }
 
 export default App
