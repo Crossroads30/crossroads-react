7fa923cce13ca755c1995b110ae54f4a5aa873fd
crossroads30 <crossroads30@mail.ru>
2024-01-30 16:42:01 +0300

    feat: add links comp with structure

---
+++ src/components/Links.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+import { links } from '../data'
+import { NavLink } from 'react-router-dom'
+
+const Links = () => {
+	return (
+		<Wrapper>
+			{links.map(link => {
+				return (
+					<li key={link.id}>
+						{/* eslint-disable-next-line
+									@ts-nocheck */}
+						<NavLink to={link.url} >
+							{link.text}
+						</NavLink>
+					</li>
+				)
+			})}
+		</Wrapper>
+	)
+}
+export default Links
+
+const Wrapper = styled.ul`
+	display: flex;
+	justify-content: space-between;
+	width: 500px;
+	li a {
+		text-decoration: none;
+		color: #000;
+		font-size: 1.2rem;
+	}
+	.active {
+		color: red;
+	}
+`
