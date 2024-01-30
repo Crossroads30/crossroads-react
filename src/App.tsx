f106aaba797ca24b78f446d30a4783973af5ff57
crossroads30 <crossroads30@mail.ru>
2024-01-31 01:03:07 +0300

    feat: add comp

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Home, Videos, Songs, Albums, News } from './pages'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import SideBar from './components/SideBar'
 
 function App(): JSX.Element {
 	return (
 		<BrowserRouter>
 			<Navbar />
+			<SideBar />
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='/videos' element={<Videos />} />
