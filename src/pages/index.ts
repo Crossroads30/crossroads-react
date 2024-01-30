b74b1e8b5d6cffbacab89f7de0f1fb32bf1bb08f
crossroads30 <crossroads30@mail.ru>
2024-01-30 15:06:36 +0300

    feat: add named imports

---
+++ src/pages/index.ts
@@ -0,0 +1,7 @@
+import Home from './Home'
+import Videos from './Videos'
+import Songs from './Songs'
+import Albums from './Albums'
+import News from './News'
+
+export {Home, Videos, Songs, Albums,News}
