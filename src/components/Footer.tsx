b6b5bf1d6765cd6e3045dff2a7319482ac22839f
crossroads30 <crossroads30@mail.ru>
2024-01-30 23:56:25 +0300

    feat: add styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,24 @@
-import styled from "styled-components"
+import styled from 'styled-components'
 
 const Footer = () => {
-  return (
-    <Wrapper>
-      <h2>footer</h2>
-    </Wrapper>
-  )
+	return (
+		<Wrapper>
+			<h2>footer</h2>
+		</Wrapper>
+	)
 }
 export default Footer
 
 const Wrapper = styled.footer`
-  display: flex;
-  align-items: center;
-  background-color: wheat;
-  height: 5rem;
-  justify-content: center;
+	display: flex;
+	align-items: center;
+	background-color: var(--clr-sepia);
+	height: 2rem;
+	justify-content: center;
+	@media screen and (min-width: 768px) {
+		height: 4rem;
+	}
+	@media screen and (min-width: 1024px) {
+		height: 5rem;
+	}
 `
