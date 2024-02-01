5ae62f5a1f61d0af57907f8d97f7357e6689ff14
crossroads30 <crossroads30@mail.ru>
2024-02-01 22:50:57 +0300

    feat: create comp

---
+++ src/pages/Song.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+const Song = () => {
+  return (
+    <Wrapper>
+      <div className='section-center flex height'>
+        
+      </div>
+    </Wrapper>
+  )
+}
+export default Song
+
+const Wrapper = styled.main`
+
+`
