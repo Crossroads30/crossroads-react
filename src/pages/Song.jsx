8101339c02d75ed8c53f0db8eb2ea11c0ced5951
crossroads30 <crossroads30@mail.ru>
2024-02-02 01:03:30 +0300

    feat: set structure

--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -1,16 +1,70 @@
-import styled from "styled-components"
+import { useParams } from 'react-router-dom'
+import styled from 'styled-components'
+import { songs } from '../data'
 
 const Song = () => {
-  return (
-    <Wrapper>
-      <div className='section-center flex height'>
-        
-      </div>
-    </Wrapper>
-  )
+	const { id } = useParams()
+
+	const singleSong = songs.find(song => song.id === Number(id))
+	const { src, url, text, title } = singleSong
+	return (
+		<Wrapper
+			style={{ background: `url(${url}) no-repeat center center / cover` }}
+		>
+			<div className='section-center height'>
+				<div className='content flex-column'>
+					<h3>{title}</h3>
+					<p className='text'>{text}</p>
+				</div>
+			</div>
+		</Wrapper>
+	)
 }
 export default Song
 
 const Wrapper = styled.main`
-
+	.content {
+		margin-top: calc(35px + 3vh);
+		align-items: center;
+		padding: 0;
+	}
+	h3 {
+		color: var(--clr-text);
+		text-shadow: 2px 2px 4px var(--clr-black);
+		margin-bottom: 1rem;
+		padding: 0.5rem 1.5rem;
+	}
+	.text {
+		color: var(--clr-text);
+		background-color: var(--clr-grey-4);
+		white-space: pre-line;
+		text-align: center;
+		font-size: 0.4rem;
+		padding: 2rem;
+		margin-bottom: 5vh;
+	}
+	@media screen and (min-width: 425px) {
+		.text {
+			font-size: 0.6rem;
+		}
+	}
+	@media screen and (min-width: 768px) {
+		.content {
+			margin-top: calc(50px + 4vh);
+		}
+		.text {
+			font-size: 0.8rem;
+		}
+	}
+	@media screen and (min-width: 1024px) {
+		.content {
+			margin-top: calc(65px + 3vh);
+		}
+		.text {
+			font-size: 1rem;
+		}
+    h3 {
+      font-size: 1.5rem;
+    }
+	}
 `
