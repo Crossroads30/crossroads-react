e156eefc2197ecabe45975d4d2457ec68174624e
crossroads30 <crossroads30@mail.ru>
2024-02-01 21:15:18 +0300

    fix: fix srtucture/styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,18 +5,17 @@ import { GiHamburgerMenu } from 'react-icons/gi'
 const Navbar = ({ setSidebarOpen }) => {
 	return (
 		<Wrapper>
-			<div className='header'>
-				<div className='section-center flex'>
-					<h1>перекрёсток</h1>
-					<div className='nav-links flex'>
-						<Links />
-					</div>
-					<GiHamburgerMenu
-						className='burger'
-						onClick={() => setSidebarOpen(true)}
-					/>
+			<div className='section-center flex'>
+				<h1>перекрёсток</h1>
+				<div className='nav-links flex'>
+					<Links />
 				</div>
+				<GiHamburgerMenu
+					className='burger'
+					onClick={() => setSidebarOpen(true)}
+				/>
 			</div>
+			<hr />
 		</Wrapper>
 	)
 }
@@ -25,53 +24,44 @@ export default Navbar
 
 const Wrapper = styled.header`
 	/* background-color: var(--clr-sepia); */
-	background-color: transparent;
+	background-color: var(--clr-grey-1);
 	position: absolute;
 	top: 0;
 	left: 0;
 	width: 100vw;
-	.header {
-		height: 2rem;
-		/* width: 90%; */
-		h1 {
-			font-size: 0.8rem;
-			text-transform: uppercase;
-			text-shadow: 3px 3px 6px var(--clr-black);
-			color: var(--clr-text-2);
-		}
-		.nav-links {
-			display: none;
-		}
+	h1 {
+		font-size: 0.8rem;
+		text-transform: uppercase;
+		text-shadow: 3px 3px 6px var(--clr-black);
+		color: var(--clr-grey-10);
+		padding: 5px 0
+	}
+	.nav-links {
+		display: none;
 	}
 	.burger {
 		cursor: pointer;
 	}
 	@media screen and (min-width: 768px) {
-		.header {
-			height: 4rem;
-			h1 {
-				font-size: 1.2rem;
-				text-transform: uppercase;
-			}
-			.burger {
-				width: 25px;
-				height: 25px;
-			}
+		h1 {
+			font-size: 1.2rem;
+			text-transform: uppercase;
+		}
+		.burger {
+			width: 25px;
+			height: 25px;
 		}
 	}
 	@media screen and (min-width: 1024px) {
-		.header {
-			height: 5rem;
-			h1 {
-				font-size: 1.5rem;
-				text-transform: uppercase;
-			}
-			.burger {
-				display: none;
-			}
-			.nav-links {
-				display: flex;
-			}
+		h1 {
+			font-size: 1.2rem;
+			text-transform: uppercase;
+		}
+		.burger {
+			display: none;
+		}
+		.nav-links {
+			display: flex;
 		}
 	}
 `
--- src/pages/Songs.jsx
+++ src/pages/Songs.jsx
@@ -8,17 +8,18 @@ const Songs = () => {
 	return (
 		<Wrapper>
 			<div className='section section-center flex height'>
-				<div className='songs flex-column'>
+				<ul className='songs flex-column'>
 					{songs.map(song => {
 						return (
-							<div key={song.id} className='song'>
+							<li key={song.id} className='song'>
 								<Link to={`/songs/:${song.id}`}>
 									<p>{song.title}</p>
+									<hr className='hr-1' />
 								</Link>
-							</div>
+							</li>
 						)
 					})}
-				</div>
+				</ul>
 			</div>
 		</Wrapper>
 	)
@@ -30,29 +31,68 @@ const Wrapper = styled.main`
 	background-size: cover;
 	.section {
 		justify-content: center;
-		align-items: center;
+		align-items: flex-start;
 	}
 	.songs {
+		margin-top: calc(35px + 5vh);
 		align-items: center;
-		/* background-color: var(--clr-grey-4); */
+		background-color: var(--clr-grey-1);
+		padding: 1.7rem;
+		max-width: calc(36vw + 20vh + 1vmin);
+		margin-bottom: calc(2%);
 	}
 	.song {
-		border-bottom: 1px solid var(--clr-white);
-		width: 100%;
+		position: relative;
+		margin-bottom: 0.7rem;
+		border-bottom: 1px solid transparent;
+		transition: transform 250ms ease-in-out;
+	}
+	.song p {
+		font-size: calc(0.2vw + 0.6vh + 1vmin);
+		color: var(--clr-grey-10);
 		text-align: center;
-		margin-bottom: 1rem;
-		p {
-			font-size: 0.9rem;
-			color: var(--clr-white);
-			margin-bottom: 0;
-		}
+		margin-bottom: 0;
+		text-transform: capitalize;
+		/* padding: 0 20px; */
+	}
+	.songs a:hover .hr-1 {
+		width: 100%;
+		visibility: visible;
+		transform: scaleX(1);
+		/* transition: all ease-in-out; */
+	}
+	.song:last-child {
+		margin-bottom: 0;
+	}
+	.hr-1 {
+		height: 1px;
+		width: 0;
+		background-color: var(--clr-grey-8);
+		position: absolute;
+		left: 0;
+		bottom: -5px;
+		transform: scaleX(0);
+		transition: transform 250ms;
+		/* transition: var(--transition); */
+		visibility: hidden;
 	}
 	@media screen and (min-width: 768px) {
+		.songs {
+			margin-top: calc(50px + 5vh);
+			width: calc(25vw + 20vh + 1vmin);
+		}
 		.song p {
-			font-size: .8rem;
+			font-size: calc(0.3vw + 0.3vh + 1vmin);
 		}
 	}
 	@media screen and (min-width: 1024px) {
+		.songs {
+			margin-top: calc(65px + 4vh);
+			width: calc(16vw + 20vh + 1vmin);
+		}
+		.song p {
+			font-size: calc(0.1vw + 0.3vh + 1vmin);
+		}
 		background: url(${background2}) no-repeat center center;
 		background-size: cover;
 	}
