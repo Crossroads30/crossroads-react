fdc05ddf6ad3088180ad0bf5a91b2ae8553c8881
crossroads30 <crossroads30@mail.ru>
2024-02-01 22:50:42 +0300

    feat: add import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Home, Videos, Songs, Albums, News } from './pages'
+import { Home, Videos, Songs, Albums, News,Song } from './pages'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import SideBar from './components/SideBar'
@@ -16,7 +16,7 @@ const App = () => {
 				<Route path='/' element={<Home />} />
 				<Route path='/videos' element={<Videos />} />
 				<Route path='/songs' element={<Songs />} />
-				<Route path='/songs/:id' element={<Songs />} />
+				<Route path='/songs/:id' element={<Song />} />
 				<Route path='/albums' element={<Albums />} />
 				<Route path='/news' element={<News />} />
 			</Routes>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,6 @@ import Videos from './Videos'
 import Songs from './Songs'
 import Albums from './Albums'
 import News from './News'
+import Song from './Song'
 
-export {Home, Videos, Songs, Albums,News}
+export { Home, Videos, Songs, Albums, News, Song }
