8bf91568d6acc4cc6b515d14a6aa14dd56718744
crossroads30 <crossroads30@mail.ru>
2024-02-05 00:18:54 +0300

    fix: remove all

--- src/features/audio/audioSlice.js
+++ src/features/audio/audioSlice.js
@@ -2,21 +2,13 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
 	isPlaying: false,
-	trackIndex: 0,
 }
 
 const audioSlice = createSlice({
 	name: 'audio',
 	initialState,
-	reducers: {
-		setPlay: state => {
-			state.isPlaying = true
-		},
-		setPause: state => {
-			state.isPlaying = false
-		},
-	},
+	reducers: {},
 })
 
-export const { setPlay, setPause, setTrackProgress } = audioSlice.actions
+// export const {} = audioSlice.actions
 export default audioSlice.reducer
