38fd9c168a73b5a283b48f925f77dfae871bebd0
crossroads30 <crossroads30@mail.ru>
2024-02-06 14:26:56 +0300

    feat: add structure with styles

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,6 +1,28 @@
+import styled from "styled-components"
+import background from '../assets/bgrs/news-bgr.png'
+
 const News = () => {
   return (
-    <h1>News</h1>
-  )
+		<Wrapper>
+			<div className='height section-center flex center'>
+				<p>Choose your own way...</p>
+			</div>
+		</Wrapper>
+	)
 }
 export default News
+
+const Wrapper = styled.main`
+	background: url(${background}) no-repeat center center;
+	background-size: cover;
+	.center {
+		justify-content: center;
+    align-items: flex-start;
+	}
+	p {
+		color: var(--clr-grey-9);
+		text-shadow: 1px 2px 4px #000000;
+    font-size: 2rem;
+    margin-top: 15rem;
+	}
+`
