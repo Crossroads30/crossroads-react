ac693b11c547116f638763bbce12b9a9c4401b14
crossroads30 <crossroads30@mail.ru>
2024-02-08 23:06:38 +0300

    feat: add data file

---
+++ src/appData/albums-data.js
@@ -0,0 +1,37 @@
+import cover1 from '../assets/bgrs/album-covers/perekrestok-cover.png'
+import cover2 from '../assets/bgrs/album-covers/man-on-moon-cover.png'
+
+export const albums = [
+	{
+		id: 'perekrestok',
+		title: 'ПЕРЕКРЁСТОК',
+		cover: cover1,
+		songs: [
+			{ id: 1, name: 'Наконец-то Рождество' },
+			{ id: 2, name: 'Человек на луне' },
+			{ id: 3, name: 'Перекрёсток' },
+			{ id: 4, name: 'Океан' },
+			{ id: 5, name: 'Колыбельная' },
+			{ id: 6, name: 'Межпланетный экспресс' },
+			{ id: 7, name: 'Под звёздами вселенной' },
+			{ id: 8, name: 'Кухня любви' },
+			{ id: 9, name: 'Когда закончится лето...' },
+			{ id: 10, name: 'Сон Алеси' },
+		],
+	},
+	{
+		id: 'chelovek-na-lune',
+		title: 'ЧЕЛОВЕК НА ЛУНЕ',
+		cover: cover2,
+		songs: [
+			{ id: 11, name: 'Я всё ещё люблю тебя' },
+			{ id: 12, name: 'Люди как куклы' },
+			{ id: 13, name: 'Солнечный Король' },
+			{ id: 14, name: 'Отель разбитых сердец' },
+			{ id: 15, name: 'Караван мечты' },
+			{ id: 16, name: 'На старый город падает снег' },
+			{ id: 17, name: 'Звёздный вальс' },
+			{ id: 18, name: 'Перерождение' },
+		],
+	},
+]
---
+++ src/appData/links-data.js
@@ -0,0 +1,7 @@
+export const links = [
+	{ id: 1, text: 'главная', url: '/' },
+	{ id: 3, text: 'песни', url: '/songs' },
+	{ id: 2, text: 'видео', url: '/videos' },
+	{ id: 4, text: 'альбомы', url: '/albums' },
+	{ id: 5, text: 'новости', url: '/news' },
+]
