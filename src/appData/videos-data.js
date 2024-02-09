f3cb9e14dd367e7d44fcda4fe8532f8aeca65ea5
crossroads30 <crossroads30@mail.ru>
2024-02-09 14:53:39 +0300

    refactor: add imports

--- src/appData/videos-data.js
+++ src/appData/videos-data.js
@@ -1,62 +1,73 @@
+import bgr1 from '../assets/bgrs/video-bgrs/ocean.png'
+import bgr2 from '../assets/bgrs/video-bgrs/alesya_dream.png'
+import bgr3 from '../assets/bgrs/video-bgrs/summer_almost_gone.png'
+import bgr4 from '../assets/bgrs/video-bgrs/man_on_a_moon.png'
+import bgr5 from '../assets/bgrs/video-bgrs/love_kitchen.png'
+import bgr6 from '../assets/bgrs/video-bgrs/under_the_stars.png'
+import bgr7 from '../assets/bgrs/video-bgrs/space_express.png'
+import bgr8 from '../assets/bgrs/video-bgrs/lullaby.png'
+import bgr9 from '../assets/bgrs/video-bgrs/crossroads.png'
+import bgr10 from '../assets/bgrs/video-bgrs/christmas.png'
+
 export const videos = [
 	{
 		id: '1',
 		name: 'Океан',
 		url: 'https://www.youtube.com/embed/fyMYqtFbAeY?autoplay=1',
-		src: 'src/assets/bgrs/video-bgrs/ocean.png',
+		src: bgr1 ,
 	},
 	{
 		id: '2',
 		name: 'Сон Алеси',
 		url: 'https://www.youtube.com/embed/1OPVJT0f_9A?autoplay=1',
-		src: 'src/assets/bgrs/video-bgrs/alesya_dream.png',
+		src: bgr2,
 	},
 	{
 		id: '3',
 		name: 'Лета почти уже нет...',
 		url: 'https://www.youtube.com/embed/La6CFeCqKZ4?autoplay=1',
-		src: 'src/assets/bgrs/video-bgrs/summer_almost_gone.png',
+		src: bgr3,
 	},
 	{
 		id: '4',
 		name: 'Человек на луне',
 		url: 'https://www.youtube.com/embed/xSQXLtW9ka0?autoplay=1',
-		src: 'src/assets/bgrs/video-bgrs/man_on_a_moon.png',
+		src: bgr4,
 	},
 	{
 		id: '5',
 		name: 'Кухня любви',
 		url: 'https://www.youtube.com/embed/qfiurXrMsFc?autoplay=1',
-		src: 'src/assets/bgrs/video-bgrs/love_kitchen.png',
+		src: bgr5,
 	},
 	{
 		id: '6',
 		name: 'Под звёздами вселенной',
 		url: 'https://www.youtube.com/embed/cV03oI4MPVU?autoplay=1',
-		src: 'src/assets/bgrs/video-bgrs/under_the_stars.png',
+		src: bgr6,
 	},
 	{
 		id: '7',
 		name: 'Межпланетный экспресс',
 		url: 'https://www.youtube.com/embed/wQF3InkqzQY?autoplay=1',
-		src: 'src/assets/bgrs/video-bgrs/space_express.png',
+		src: bgr7,
 	},
 	{
 		id: '8',
 		name: 'Колыбельная',
 		url: 'https://www.youtube.com/embed/NqpizZjW54U?autoplay=1',
-		src: 'src/assets/bgrs/video-bgrs/lulaby.png',
+		src: bgr8,
 	},
 	{
 		id: '9',
 		name: 'Перекрёсток',
 		url: 'https://www.youtube.com/embed/bz01x0xPis4?autoplay=1',
-		src: 'src/assets/bgrs/video-bgrs/crossroads.png',
+		src: bgr9,
 	},
 	{
 		id: '10',
 		name: 'Наконец-то Рождество',
 		url: 'https://www.youtube.com/embed/vw-4y3bPLlw?autoplay=1',
-		src: 'src/assets/bgrs/video-bgrs/сhristmas.png',
+		src: bgr10,
 	},
 ]
